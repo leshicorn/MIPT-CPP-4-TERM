9f8a065cf5e200a98a88645647162131d2d8fa50
leshicorn <romanenkov2003@mail.ru>
2024-05-18 17:23:40 +0300

    second task solution

--- kr2/task2/Common.h
+++ kr2/task2/Common.h
@@ -1,22 +1,21 @@
 #pragma once
-
 #include <memory>
 #include <string>
 
 // Базовый класс арифметического выражения
 class Expression {
-public:
-  virtual ~Expression() = default;
+    public: virtual~Expression() =
+        default;
 
-  // Вычисляет значение выражения
-  virtual int Evaluate() const = 0;
+    // Вычисляет значение выражения
+    virtual int Evaluate() const = 0;
 
-  // Форматирует выражение как строку
-  // Каждый узел берётся в скобки, независимо от приоритета
-  virtual std::string ToString() const = 0;
+    // Форматирует выражение как строку
+    // Каждый узел берётся в скобки, независимо от приоритета
+    virtual std::string ToString() const = 0;
 };
 
-using ExpressionPtr = std::unique_ptr<Expression>;
+using ExpressionPtr = std::unique_ptr < Expression > ;
 
 // Функции для формирования выражения
 ExpressionPtr Value(int value);
